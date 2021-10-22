c2280fa84ee98a68821b46f8e00236040306c1c8
Collin Bardini <cbardini@osisoft.com>
2021-10-22 08:30:11 -0700

    Version1 (#1)
    * Useful files
    
    * Create program.py
    
    * Update program.py
    
    * Pipelines and testing
    
    * Update README.md
    
    * Updates
    
    * Update README.md
    
    * Update azure-pipelines.yml
    
    * address comments
    
    * added testing
    
    * address comments
    
    * Update program.py
    
    * Update program.py

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# sample-ocs-security_management-python
+# OSIsoft Cloud Services Security Management Python Sample
+
+**Version:** 1.0.0
+
+[![Build Status](https://dev.azure.com/osieng/engineering/_apis/build/status/product-readiness/OCS/osisoft.sample-ocs-security_management-python?repoName=osisoft%2Fsample-ocs-security_management-python&branchName=main)](https://dev.azure.com/osieng/engineering/_build/latest?definitionId=4027&repoName=osisoft%2Fsample-ocs-security_management-python&branchName=main)
+
+Developed against Python 3.9.1.
+
+## Requirements
+
+- Python 3.9+
+- Register a [Client-Credentials Client](https://cloud.osisoft.com/clients) in your OSIsoft Cloud Services tenant and create a client secret to use in the configuration of this sample. ([Video Walkthrough](https://www.youtube.com/watch?v=JPWy0ZX9niU))
+- Install required modules: `pip install -r requirements.txt`
+
+## About this sample
+
+This sample uses the sample python library, which makes REST API calls to OCS, to manage the security of an OCS Namespace and Tenant. The steps are as follows
+
+1. Create a custom role
+1. Create a user and invite them to the Tenant
+1. Create a Type
+1. Create a Stream
+1. Add the created role to the created stream's access control list (ACL) using a PUT REST call
+1. Add the 'Tenant Member' role to the created stream's access control list (ACL) using a PATCH REST call
+1. Change the owner of the created stream
+1. Retrieve the access rights of the example stream
+1. Verify the results of the above steps
+1. Cleanup the created stream, type, role, and user
+
+## Configuring the sample
+
+The sample is configured by modifying the file [appsettings.placeholder.json](appsettings.placeholder.json). Details on how to configure it can be found in the sections below. Before editing appsettings.placeholder.json, rename this file to `appsettings.json`. This repository's `.gitignore` rules should prevent the file from ever being checked in to any fork or branch, to ensure credentials are not compromised.
+
+### Configuring appsettings.json
+
+OSIsoft Cloud Services is secured by obtaining tokens from its identity endpoint. Client credentials clients provide a client application identifier and an associated secret (or key) that are authenticated against the token endpoint. You must replace the placeholders in your `appsettings.json` file with the authentication-related values from your tenant and a client-credentials client created in your OCS tenant.
+
+```json
+{
+  "Resource": "https://dat-b.osisoft.com",
+  "ApiVersion": "v1",
+  "TenantId": "PLACEHOLDER_REPLACE_WITH_TENANT_ID",
+  "NamespaceId": "PLACEHOLDER_REPLACE_WITH_NAMESPACE_ID",
+  "CommunityId": null,
+  "ClientId": "PLACEHOLDER_REPLACE_WITH_APPLICATION_IDENTIFIER",
+  "ClientSecret": "PLACEHOLDER_REPLACE_WITH_APPLICATION_SECRET",
+  "ContactGivenName": "PLACEHOLDER_REPLACE_WITH_CONTACT_GIVEN_NAME",
+  "ContactSurname": "PLACEHOLDER_REPLACE_WITH_CONTACT_SURNAME",
+  "ContactEmail": "PLACEHOLDER_REPLACE_WITH_CONTACT_EMAIL"
+}
+```
+
+## Running the sample
+
+To run this example from the command line once the `appsettings.json` is configured, run
+
+```shell
+python program.py
+```
+
+## Running the automated test
+
+To test the sample, run
+
+```shell
+pip install pytest
+python -m pytest test.py
+```
+
+---
+
+Tested against Python 3.9.1
+
+For the main OCS samples page [ReadMe](https://github.com/osisoft/OSI-Samples-OCS)  
+For the main OSIsoft samples page [ReadMe](https://github.com/osisoft/OSI-Samples)
---
+++ program.py
@@ -0,0 +1,195 @@
+import json
+import jsonpatch
+import traceback
+
+from ocs_sample_library_preview import (OCSClient, Role, RoleScope, Trustee, TrusteeType, User, UserInvitation, AccessControlList,
+                                        AccessControlEntry, AccessType, CommonAccessRightsEnum, SdsType, SdsTypeProperty, SdsTypeCode, SdsStream)
+
+custom_role_name = 'custom role - security management sample'
+
+def get_appsettings():
+    """Open and parse the appsettings.json file"""
+
+    # Try to open the configuration file
+    try:
+        with open(
+            'appsettings.json',
+            'r',
+        ) as f:
+            appsettings = json.load(f)
+    except Exception as error:
+        print(f'Error: {str(error)}')
+        print(f'Could not open/read appsettings.json')
+        exit()
+
+    return appsettings
+
+def suppress_error(sds_call):
+    """Suppress an error thrown by SDS"""
+    try:
+        sds_call()
+    except Exception as error:
+        print(f'Encountered Error: {error}')
+
+
+def get_tenant_member_role_id(client: OCSClient):
+    """Helper function that retrieves the first role with the Tenant Member role type Id"""
+    roles = client.Roles.getRoles()
+    for role in roles:
+        if role.RoleTypeId == client.Roles.TenantMemberRoleTypeId:
+            return role.Id
+
+
+def compare_acls(acl1: AccessControlList, acl2: AccessControlList):
+    """Helper function for comparing two access control lists"""
+    if not (len(acl1.RoleTrusteeAccessControlEntries) == len(acl2.RoleTrusteeAccessControlEntries)):
+        return False
+
+    for ace1 in acl1.RoleTrusteeAccessControlEntries:
+        ace_match = False
+        for ace2 in acl2.RoleTrusteeAccessControlEntries:
+            if ace1.AccessType == ace2.AccessType and \
+                    ace1.Trustee.ObjectId == ace2.Trustee.ObjectId and \
+                    ace1.Trustee.TenantId == ace2.Trustee.TenantId and \
+                    ace1.Trustee.Type == ace2.Trustee.Type and \
+                    ace1.AccessRights == ace2.AccessRights:
+                ace_match = True
+                break
+        if not ace_match:
+            return False
+
+    return True
+
+
+def main():
+    """This function is the main body of the security sample script"""
+    global custom_role_name
+
+    try:
+        print('Sample starting...')
+
+        # Read appsettings and create a client
+        appsettings = get_appsettings()
+
+        tenant_id = appsettings.get('TenantId')
+        namespace_id = appsettings.get('NamespaceId')
+        contact_given_name = appsettings.get('ContactGivenName')
+        contact_surname = appsettings.get('ContactSurname')
+        contact_email = appsettings.get('ContactEmail')
+
+        client = OCSClient(appsettings.get('Access', 'ApiVersion'),
+                           appsettings.get('Access', 'TenantId'),
+                           appsettings.get('Access', 'Resource'),
+                           appsettings.get('Credentials', 'ClientId'),
+                           appsettings.get('Credentials', 'ClientSecret'))
+
+        # Step 1
+        print('Creating a role')
+        custom_role = Role(name=custom_role_name,
+                           role_scope=RoleScope.Tenant, tenant_id=tenant_id)
+        custom_role = client.Roles.createRole(custom_role)
+
+        # Step 2
+        print('Creating a user and invite them')
+        user = User(contact_given_name=contact_given_name, contact_surname=contact_surname, contact_email=contact_email,
+                    identity_provider_id=client.Users.MicrosoftIdentityProviderId, role_ids=[custom_role.Id])
+
+        user.RoleIds.append(get_tenant_member_role_id(client))
+        user = client.Users.createUser(user)
+        invitation = UserInvitation(send_invitation=True)
+        client.Users.createOrUpdateInvitation(user.Id, invitation)
+
+        # Step 3
+        print('Creating a type')
+        date_time_type = SdsType('DateTimeType', SdsTypeCode.DateTime)
+        int_type = SdsType('IntType', SdsTypeCode.Int32)
+        date_time_property = SdsTypeProperty('DateTime', True, date_time_type)
+        int_property = SdsTypeProperty('Value', False, int_type)
+        example_type = SdsType('example_type-security_management_sample', SdsTypeCode.Object, [
+            date_time_property, int_property], 'This is a type example.')
+        example_type = client.Types.getOrCreateType(namespace_id, example_type)
+
+        # Step 4
+        print('Creating a stream')
+        example_stream = SdsStream(
+            'example_stream-security_management_sample', example_type.Id)
+        example_stream = client.Streams.getOrCreateStream(
+            namespace_id, example_stream)
+
+        # Step 5
+        print(
+            'Adding custom role to example type and stream access control lists using PUT')
+        trustee = Trustee(TrusteeType.Role, tenant_id, custom_role.Id)
+        entry = AccessControlEntry(trustee, AccessType.Allowed,
+                                   CommonAccessRightsEnum.Read | CommonAccessRightsEnum.Write)
+
+        type_acl = client.Types.getAccessControl(
+            namespace_id, example_type.Id)
+        type_acl.RoleTrusteeAccessControlEntries.append(entry)
+        client.Types.updateAccessControl(
+            namespace_id, example_type.Id, type_acl)
+
+        stream_acl = client.Streams.getAccessControl(
+            namespace_id, example_stream.Id)
+        stream_acl.RoleTrusteeAccessControlEntries.append(entry)
+        client.Streams.updateAccessControl(
+            namespace_id, example_stream.Id, stream_acl)
+
+        # Step 6
+        print('Adding a role from the example stream access control list using PATCH')
+        patch = jsonpatch.JsonPatch(
+            [{
+                'op': 'add', 'path': '/RoleTrusteeAccessControlEntries/-',
+                'value': {
+                    'AccessRights': 0,
+                    'AccessType': 'Allowed',
+                    'Trustee': {'ObjectId': get_tenant_member_role_id(client), 'TenantId': tenant_id, 'Type': 'Role'}
+                }
+            }])
+        client.Streams.patchAccessControl(
+            namespace_id, example_stream.Id, patch)
+
+        # Step 7
+        print('Changing owner of example stream')
+        stream_owner = client.Streams.getOwner(namespace_id, example_stream.Id)
+        stream_owner.ObjectId = user.Id
+        stream_owner.Type = TrusteeType.User
+        client.Streams.updateOwner(
+            namespace_id, example_stream.Id, stream_owner)
+
+        # Step 8
+        print('Retrieving the access rights of the example stream')
+        access_rights = client.Streams.getAccessRights(
+            namespace_id, example_stream.Id)
+        for access_right in access_rights:
+            print(access_right.name)
+        
+        # Step 9
+        print('Verifying the results of the previous steps')
+        trustee = Trustee(TrusteeType.Role, tenant_id, get_tenant_member_role_id(client))
+        entry = AccessControlEntry(trustee, AccessType.Allowed, CommonAccessRightsEnum.none)
+        stream_acl.RoleTrusteeAccessControlEntries.append(entry)
+        assert compare_acls(client.Types.getAccessControl(namespace_id, example_type.Id), type_acl)
+        assert compare_acls(client.Streams.getAccessControl(namespace_id, example_stream.Id), stream_acl)
+        assert client.Streams.getOwner(namespace_id, example_stream.Id).ObjectId == stream_owner.ObjectId
+
+    except Exception as error:
+        print((f'Encountered Error: {error}'))
+        print()
+        traceback.print_exc()
+        print()
+
+    finally:
+        # Step 10
+        print('Cleaning Up')
+        suppress_error(lambda: client.Streams.deleteStream(namespace_id, example_stream.Id))
+        suppress_error(lambda: client.Types.deleteType(namespace_id, example_type.Id))
+        suppress_error(lambda: client.Roles.deleteRole(custom_role.Id))
+        suppress_error(lambda: client.Users.deleteUser(user.Id))
+
+    print('Complete!')
+
+
+if __name__ == '__main__':
+
+    main()
